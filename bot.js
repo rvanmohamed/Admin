db3148060622e4e0fb0116c085a3fe64dce2b6a6
rvanmohamed <49373527+rvanmohamed@users.noreply.github.com>
2019-04-07 21:45:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,4 +11,28 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', message => {
+    if (message.content.startsWith("!avatar")) {
+        var mentionned = message.mentions.users.first(); //by  BlackOps :D
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+
+client.on('message', msg => {
+  if (msg.content === 'hi') {
+    msg.reply('hi!');
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
