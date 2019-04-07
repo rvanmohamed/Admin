f3cd3e6fa46fde38a6bc6954c422a825e3af3907
rvanmohamed <49373527+rvanmohamed@users.noreply.github.com>
2019-04-07 23:40:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on('ready', () = {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
+client.on('message', msg = {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
 });
 
-client.on('message', message => {
-    if (message.content.startsWith("!avatar")) {
-        var mentionned = message.mentions.users.first(); //by  KePo :D
+client.on('message', message = {
+    if (message.content.startsWith(!avatar)) {
+        var mentionned = message.mentions.users.first(); by  BlackOps D
     var x5bzm;
       if(mentionned){
           var x5bzm = mentionned;
@@ -22,16 +22,72 @@ client.on('message', message => {
           
       }
         const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
+        .setColor(RANDOM)
         .setImage(`${x5bzm.avatarURL}`)
       message.channel.sendEmbed(embed);
     }
 });
 
 
-client.on('message', msg => {
+client.on('message', msg = {
   if (msg.content === 'hi') {
-    msg.reply('Welcome Pro <3!');
+    msg.reply('Welcome Pro To Server!');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
+
+
+
+
+client.on('message', message = {
+    if (message.content.startsWith(!avatar)) {
+        var mentionned = message.mentions.users.first(); by  BlackOps D
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor(RANDOM)
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+
+client.on('message', msg = {
+  if (msg.content === 'كسمك') {
+    msg.reply('ابويا نك امك!');
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
+
+
+client.on('message', message = {
+    if (message.content.startsWith(!avatar)) {
+        var mentionned = message.mentions.users.first(); by  BlackOps D
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor(RANDOM)
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+
+client.on('message', msg = {
+  if (msg.content === '-help') {
+    msg.reply('mmkn 7drtk tklm 7d mn el STAFF fe el Gourp 4rkan Welcome Pro!');
   }
 });
 
