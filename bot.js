2ace9eae4278c5555ba8ce359e88b92bde8ce595
rvanmohamed <49373527+rvanmohamed@users.noreply.github.com>
2019-04-08 13:23:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,26 +6,26 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+if (msg.content === 'ping') {
+msg.reply('Pong!');
+}
 });
 
 client.on('message', message => {
-    if (message.content.startsWith("!avatar")) {
-        var mentionned = message.mentions.users.first(); //by  BlackOps :D
-    var x5bzm;
-      if(mentionned){
-          var x5bzm = mentionned;
-      } else {
-          var x5bzm = message.author;
-          
-      }
-        const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        .setImage(`${x5bzm.avatarURL}`)
-      message.channel.sendEmbed(embed);
-    }
+if (message.content.startsWith("!avatar")) {
+var mentionned = message.mentions.users.first(); //by  BlackOps :D
+var x5bzm;
+if(mentionned){
+var x5bzm = mentionned;
+} else {
+var x5bzm = message.author;
+}    
+      
+const embed = new Discord.RichEmbed()
+.setColor("RANDOM")
+.setImage(`${x5bzm.avatarURL}`)
+message.channel.sendEmbed(embed);
+ }   
 });
 
 
